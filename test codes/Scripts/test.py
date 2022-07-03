c85ef861ea393d09cf41cf19778dd25be52e7cd0
sergeiovs <mr.serz98@mail.ru>
2022-07-03 15:23:46 -0700

    123

--- test codes/Scripts/test.py
+++ test codes/Scripts/test.py
@@ -1,47 +1,22 @@
-import pygame as pg
+from openpyxl import load_workbook
+
+# Load in the workbook
+wb = load_workbook('расчет.xlsx')
+data = 'My'
+# Get sheet names
+wb.create_sheet(data)
+print(wb.get_sheet_names())
 
 
-class Entity(pg.sprite.Sprite):
+"""
 
-    def __init__(self, pos):
-        super().__init__()
-        self.image = pg.Surface((122, 70), pg.SRCALPHA)
-        pg.draw.polygon(self.image, pg.Color('dodgerblue1'),((1, 0), (120, 35), (1, 70)))
+import openpyxl
+wb = openpyxl.reader.excel.load_workbook(filename="sample.xlsx", data_only=True)
+print(wb.sheetnames)
+wb.active = 1
+sheet = wb.active
+#print(sheet['A1'].value)
+for i in range(1,12):
+    print(sheet['A'+str(i)].value,sheet['B'+str(i)].value,sheet['C'+str(i)].value)
 
-
-        # A reference to the original image to preserve the quality.
-
-        self.orig_image = self.image
-        self.rect = self.image.get_rect(center=pos)
-        self.angle = 0
-
-    def update(self):
-        self.angle -= 5
-        self.rotate()
-
-    def rotate(self):
-        """Rotate the image of the sprite around its center."""
-        # `rotozoom` usually looks nicer than `rotate`. Pygame rotation
-        # functions return new images and don't modify the originals.
-        self.image = pg.transform.rotozoom(self.orig_image, self.angle, 1)
-        # Create a new rect with the center of the old rect.
-        self.rect = self.image.get_rect(center=self.rect.center)
-
-
-def main():
-    screen = pg.display.set_mode((640, 480))
-    clock = pg.time.Clock()
-    all_sprites = pg.sprite.Group(Entity((320, 240)))
-
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                return
-
-        all_sprites.update()
-        screen.fill((30, 30, 30))
-        all_sprites.draw(screen)
-        pg.display.flip()
-        clock.tick(30)
-
-main()
+"""
