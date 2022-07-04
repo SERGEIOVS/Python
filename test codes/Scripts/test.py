e14ef7c89e7ba4b16b4d1d0cc1267929134fdd56
sergeiovs <mr.serz98@mail.ru>
2022-07-03 18:57:21 -0700

    123

--- test codes/Scripts/test.py
+++ test codes/Scripts/test.py
@@ -1,22 +1,19 @@
+from typing import List
 from openpyxl import load_workbook
 
+List1 = [ 'ttt' , 'b' , 'a' ]
+
 # Load in the workbook
-wb = load_workbook('расчет.xlsx')
-data = 'My'
-# Get sheet names
-wb.create_sheet(data)
-print(wb.get_sheet_names())
-
-
-"""
-
-import openpyxl
-wb = openpyxl.reader.excel.load_workbook(filename="sample.xlsx", data_only=True)
-print(wb.sheetnames)
+wb = load_workbook('123.xlsx')
 wb.active = 1
-sheet = wb.active
-#print(sheet['A1'].value)
-for i in range(1,12):
-    print(sheet['A'+str(i)].value,sheet['B'+str(i)].value,sheet['C'+str(i)].value)
 
-"""
+sheet = wb.active
+
+# Get sheet names
+
+for i in range(1,len(List1)):
+    sheet['A'+str(i)].value =str(List1[i])
+    print(sheet['A'+str(i)].value)
+    
+wb.save('123.xlsx')
+
